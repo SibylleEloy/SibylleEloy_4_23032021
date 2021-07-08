af4433791455b332d65d78d541fcb995b165e2bc
Sibylle Eloy <sibylleeloy@hotmail.com>
2021-07-08 15:01:12 +0200

    gulp set up

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,23 +1,51 @@
-let gulp = require("gulp");
-let cleanCSS = require("gulp-clean-css");
-// Task to minify css using package cleanCSs
-gulp.task("minify-css", () => {
-  // Folder with files to minify
-  return (
-    gulp
-      .src("styles/style.css")
-      //The method pipe() allow you to chain multiple tasks together
-      //I execute the task to minify the files
-      .pipe(cleanCSS())
-      //I define the destination of the minified files with the method dest
-      .pipe(gulp.dest("dist"))
-  );
-});
+const gulp = require("gulp");
+const imagemin = require("gulp-imagemin");
+const concat = require("gulp-concat");
+const terser = require("gulp-terser");
+const sourcemaps = require("gulp-sourcemaps");
+const postcss = require("gulp-postcss");
+const cssnano = require("cssnano");
+const autoprefixer = require("autoprefixer");
+const { src, series, parallel, dest, watch } = require("gulp");
 
-//We create a 'default' task that will run when we run `gulp` in the project
-gulp.task("default", function () {
-  // We use `gulp.watch` for Gulp to expect changes in the files to run again
-  gulp.watch("./styles/style.css", function (evt) {
-    gulp.task("minify-css");
-  });
-});
+const jsPath = "src/assets/js/**/*.js";
+const cssPath = "src/assets/css/**/*.css";
+
+function copyHtml() {
+  return src("src/*.html").pipe(gulp.dest("dist"));
+}
+
+function imgTask() {
+  return src("src/images/*").pipe(imagemin()).pipe(gulp.dest("dist/images"));
+}
+
+function jsTask() {
+  return src(jsPath)
+    .pipe(sourcemaps.init())
+    .pipe(concat("all.js"))
+    .pipe(terser())
+    .pipe(sourcemaps.write("."))
+    .pipe(dest("dist/assets/js"));
+}
+
+function cssTask() {
+  return src(cssPath)
+    .pipe(sourcemaps.init())
+    .pipe(concat("style.css"))
+    .pipe(postcss([autoprefixer(), cssnano()])) //not all plugins work with postcss only the ones mentioned in their documentation
+    .pipe(sourcemaps.write("."))
+    .pipe(dest("dist/assets/css"));
+}
+
+function watchTask() {
+  watch([cssPath, jsPath], { interval: 1000 }, parallel(cssTask, jsTask));
+}
+
+exports.cssTask = cssTask;
+exports.jsTask = jsTask;
+exports.imgTask = imgTask;
+exports.copyHtml = copyHtml;
+exports.default = series(
+  parallel(copyHtml, imgTask, jsTask, cssTask),
+  watchTask
+);
